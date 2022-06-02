131b7b298ccfdcef9c51a3cced3ab949289eb733
Jose <jlconde3@gmail.com>
2022-06-02 11:21:19 +0200

    Se ha incluido DOM

--- index.js
+++ index.js
@@ -1,49 +1,74 @@
 
 
-Game("rock", ComputerPlay());
+const btnrock = document.querySelector('#btnrock'); 
+btnrock.addEventListener("click",SelectionRock);
 
-function ComputerPlay (){
-
-   let  computerchoicevalue = Math.floor (Math.random()*2);
-   let computerchoice;
-
-    switch (computerchoicevalue){
-        case 0:
-            computerchoice = "rock";
-            break;
-        case 1:
-            computerchoice = "paper";
-            break;
-        case 2: 
-            computerchoice = "scissors";
-            break;
-    }
-
-    return computerchoice;
+function SelectionRock(){
+  Game("rock",ComputerPlay());
 }
 
+const btnpaper = document.querySelector('#btnpaper'); 
+btnpaper.addEventListener("click",SelectionPaper);
+
+function SelectionPaper(){
+  Game("paper",ComputerPlay());
+}
+
+const btnscissors = document.querySelector('#btnscissors'); 
+btnscissors.addEventListener("click",SelectionScissors);
+
+function SelectionScissors(){
+  Game("scissors",ComputerPlay());
+}
+
+
+function ComputerPlay (){
+  let  computerchoicevalue = Math.floor (Math.random()*3);
+  let computerchoice;
+
+  switch (computerchoicevalue){
+    case 0:
+      computerchoice = "rock";
+      break;
+    case 1:
+      computerchoice = "paper";
+      break;
+    case 2: 
+      computerchoice = "scissors";
+      break;
+  }
+  return computerchoice;
+}
 
 
 function Game(playerselection, computerchoice){
-    let result;
-    if (playerselection =="rock" && computerchoice == "scissors"){result = 1; console.log(ResultText(playerselection, computerchoice, result));}
-    else if (playerselection =="paper" && computerchoice == "rock"){result = 1; console.log(ResultText(playerselection, computerchoice, result));}
-    else if (playerselection =="scissors" && computerchoice == "paper"){result = 1; console.log(ResultText(playerselection, computerchoice, result));}
-    else if (playerselection =="rock" && computerchoice == "paper"){result = 0; console.log(ResultText(playerselection, computerchoice, result))}
-    else if (playerselection =="paper" && computerchoice == "scissors"){result = 0; console.log(ResultText(playerselection, computerchoice, result));}
-    else if (playerselection =="scissors" && computerchoice == "rock"){result = 0; console.log(ResultText(playerselection, computerchoice, result));}
-    else if (playerselection == computerchoice){result = 2; console.log(ResultText(playerselection, computerchoice, result));}
+  let result;
+  if (playerselection =="rock" && computerchoice == "scissors"){result = 1; console.log(ResultText(result));}
+  else if (playerselection =="paper" && computerchoice == "rock"){result = 1; console.log(ResultText(result));}
+  else if (playerselection =="scissors" && computerchoice == "paper"){result = 1; console.log(ResultText(result));}
+  else if (playerselection =="rock" && computerchoice == "paper"){result = 0; console.log(ResultText(result));}
+  else if (playerselection =="paper" && computerchoice == "scissors"){result = 0; console.log(ResultText(result));}
+  else if (playerselection =="scissors" && computerchoice == "rock"){result = 0; console.log(ResultText(result));}
+  else if (playerselection == computerchoice){result = 2; console.log(ResultText(result));}
 }
  
-function ResultText (playerselection, computerchoice,result){
-    switch (result){
-        case 0:
-            return "You loose! " + computerchoice + " beats " + playerselection;
-        case 1:
-            return "You win! " + playerselection + " beats " + computerchoice;
-        case 2:
-            return "Draw!! " + playerselection + " equals to " + computerchoice;
-    }
+function ResultText (result){
 
+  const container = document.querySelector('#Resultado');
+  const content = document.createElement('div');  
+
+  switch (result){
+    case 0:
+      content.textContent = 'You win!'; 
+      container.appendChild(content);
+      break;
+    case 1:
+      content.textContent = 'You loose!'; 
+      container.appendChild(content);
+      break;
+    case 2:
+      content.innerHTML = '<h4>Draw!</h4>'; 
+      container.appendChild(content);
+      break;
+  }
 }
-
